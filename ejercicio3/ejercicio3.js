89fb681c30b36813648847522ca614e130f3cb1e
MelanieOC <melanie.ocharan@gmail.com>
2017-07-18 15:04:04 -0500

    algo de CSS

--- ejercicio3/ejercicio3.js
+++ ejercicio3/ejercicio3.js
@@ -1,5 +1,11 @@
-function convertir() {
+function convertirFaC() {
   var fahrenheit = document.getElementById('fahrenheit').value;
-  var celsius=(fahrenheit - 32)*(5/9);
-  document.getElementById('celsius').innerHTML = celsius.toFixed(2);
+  var celsius=(fahrenheit - 32)/1.8;
+  document.getElementById('celsius').innerHTML= celsius.toFixed(2);
+}
+
+function convertirCaF() {
+  var celsius = document.getElementById('celsius2').value;
+  var fahrenheit = celsius * 1.8 + 32;
+  document.getElementById('fahrenheit2').innerHTML= fahrenheit.toFixed(2);
 }
